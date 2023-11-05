558037e4a9e3f01a8a6e9112f2060395fe59bf53
noveminfo <noveminfo@gmail.com>
2023-11-05 20:03:06 +0900

    feat: server sidebar

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -5,6 +5,8 @@ import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
 import { X } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
+import { Skeleton } from "./ui/skeleton";
 
 type Props = {
   onChange: (url?: string) => void;
@@ -14,16 +16,19 @@ type Props = {
 
 export const FileUpload = ({ onChange, value, endpoint }: Props) => {
   const fileType = value?.split(".").pop();
+  const [isLoaded, setIsLoaded] = useState(true);
 
   if (value && fileType !== "pdf") {
     return (
       <div className="relative h-20 w-20">
+        {isLoaded && <Skeleton className="h-20 w-20 rounded-full" />}
         <Image
           fill
           src={value}
           alt="Upload"
           className="rounded-full"
           sizes="100%"
+          onLoadingComplete={() => setIsLoaded(false)}
         />
         <button
           className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
