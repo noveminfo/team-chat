a210948d27f7e15c6ac4d36b6468d3e3917c0e9a
noveminfo <noveminfo@gmail.com>
2024-01-07 14:41:54 +0900

    feat: delete, leave server

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -6,6 +6,8 @@ import { InviteModal } from "../modals/invite-modal";
 import { MembersModal } from "../modals/members-modal";
 import { EditServerModal } from "../modals/edit-server-modal";
 import { CreateChannelModal } from "../modals/create-channel-modal";
+import { LeaveServerModal } from "../modals/leave-server-modal";
+import { DeleteServerModal } from "../modals/delete-server-modal";
 
 export const ModalProvider = () => {
   const isMounted = useIsMounted();
@@ -21,6 +23,8 @@ export const ModalProvider = () => {
       <EditServerModal />
       <MembersModal />
       <CreateChannelModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
     </>
   );
 };
